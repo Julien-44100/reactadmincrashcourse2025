ba258a1dc0828885c6bb92910d406e8e25bf51b5
Julien-44100 <julienbotar44100@yahoo.com>
2025-01-16 15:34:07 +0100

    add PostPanel in the file post-list.tsx

--- react-admin-demo/src/pages/posts/post-list.tsx
+++ react-admin-demo/src/pages/posts/post-list.tsx
@@ -1,11 +1,31 @@
-import { Datagrid, List, ReferenceField, TextField, FunctionField } from "react-admin";
+import {
+  Datagrid,
+  List,
+  ReferenceField,
+  TextField,
+  FunctionField,
+} from "react-admin";
+
+const PostPanel = () => {
+  return <div>Hello</div>;
+};
 
 const PostList = () => (
   <List>
-    <Datagrid>
+    <Datagrid
+      expand={<PostPanel />}
+      sx={{
+        "& .RaDatagrid-headerCell": {
+          padding: "16px",
+        },
+      }}
+    >
       <TextField source="id" />
-      <TextField source="title" label=" Post title" />
-        <FunctionField label="Excerpt" render={(record) => }/>
+      <TextField source="title" label="Post Title" />
+      <FunctionField
+        label="Excerpt"
+        render={(record) => `${record.body.substring(0, 50)}...`}
+      />
       <ReferenceField source="userId" reference="users" />
     </Datagrid>
   </List>
